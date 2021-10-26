ec921864142215de44758c023f542e83c362a9e9
Chayti Saha <chaytisaha98@gmail.com>
2021-10-26 14:03:36 +0600

    single service data load from DB

--- src/Pages/Booking/Booking/Booking.js
+++ src/Pages/Booking/Booking/Booking.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const Booking = () => {
     const { serviceId } = useParams();
+    const [service, setService] = useState({})
+    useEffect(() => {
+        fetch(`http://localhost:5000/services/${serviceId}`)
+            .then(res => res.json())
+            .then(data => setService(data))
+    }, [])
     return (
         <div>
-            <h2>this is booking: {serviceId}</h2>
+            <h2 className="fw-bold text-success mt-4">Details of : {service.name}</h2>
+            <h5 className="text-muted m-3">Service Id: {serviceId}</h5>
+            <img src={service.img} alt={service.name} />
+            <p className="w-25 mx-auto">{service.description}</p>
+            <p className="w-25 mx-auto">Price: ${service.price}</p>
         </div>
     );
 };
